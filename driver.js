8406c4aa43bac9b5df22708eac23d0b159987757
Marcus Twichel <marc.twichy@gmail.com>
2019-03-21 18:57:37 -0600

    changed timing logic and added param for location

--- driver.js
+++ driver.js
@@ -7,32 +7,41 @@ admin.initializeApp({
 
 var db = admin.firestore();
 
+var locationId = process.argv[2];
+
+if (locationId === undefined) {
+  console.error('You need to enter a locationId');
+  process.exit(1);
+}
+
+var lastDate;
+
 (function loop() {
   setTimeout(function () {
-    var currentDt = new Date();
-    var dt = new Date(currentDt.getTime() - 2000)
-    var date = dt.getFullYear() + '-' +
-    ("0" + (dt.getMonth()+1)).slice(-2) + '-' +
-    ("0" + dt.getDate()).slice(-2) + 'T' +
-    ("0" + dt.getHours()).slice(-2) + ':' +
-    ("0" + dt.getMinutes()).slice(-2) + ':' +
-    ("0" + (dt.getSeconds())).slice(-2) + 'Z';
-    console.log(date);
+    if(lastDate === undefined) {
+      var currentDt = new Date();
+      var dt = new Date(currentDt.getTime() - 3000)
+      lastDate = dt.toISOString();
+    }
+
+    console.log(lastDate, new Date().toISOString());
     const options = {
-      url: 'https://connect.squareup.com/v1/YWX343F40AM19/payments?begin_time=' + date,
+      url: 'https://connect.squareup.com/v1/'+ locationId + '/payments?begin_time=' + lastDate,
       headers: {
         'Authorization': 'Bearer sq0atp-ZZt4RfHILmH2Bx3jqHXR5g'
       }
     };
 
+    var currentDt = new Date().toISOString();
+
     request(options, function (error, response, body) {
       var obj = JSON.parse(body);
       if (obj.length !== 0) {
         for (var orderIndex=0; orderIndex<obj.length; orderIndex++){
-          console.log("Order " + orderIndex);
+          //console.log("Order " + orderIndex);
           var orderObj = obj[orderIndex];
           for(var itemIndex = 0; itemIndex < orderObj.itemizations.length; itemIndex++) {
-            console.log("Item " + itemIndex);
+            //console.log("Item " + itemIndex);
             var itemObj = orderObj.itemizations[itemIndex];
 
             var modifiers = itemObj.modifiers.map((item) => item.name);
@@ -41,13 +50,16 @@ var db = admin.firestore();
               var labelObj = {
                 title: itemObj.name,
                 modifiers: modifiers,
-                size: itemObj.item_variation_name
+                size: itemObj.item_variation_name,
+                time: itemObj.created_at,
+                bumped: false
               };
               db.collection('items').doc('items').collection('YWX343F40AM19').add(labelObj).then((ref) => {
                 console.log('Added document with ID: ', ref.id);
               }).catch((err) => {
                 console.error(err);
               });
+
             }
           }
         }
